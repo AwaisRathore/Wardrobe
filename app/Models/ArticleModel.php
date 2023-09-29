1654711a6ad20924e443ea75601ad8cbe6ee21c5
Umer-Sudozai9896 <sardarumer95@gmail.com>
2023-09-29 22:21:16 +0500

    Update ArticleModel.php

--- app/Models/ArticleModel.php
+++ app/Models/ArticleModel.php
@@ -129,23 +129,24 @@ class ArticleModel extends Model
         return true;
     }
 
-    private function handleArticleInCluster($article, $insertedArticleClusterId, $clusterModel)
-    {
-        $clusterBroken = false;
-        $this->shouldBreakCluster($article, $insertedArticleClusterId, $clusterModel);
-        if ($this->shouldBreakCluster($article, $insertedArticleClusterId, $clusterModel)) {
+    private function handleArticleInCluster($article, $insertedArticleClusterId, $clusterModel) {
+        $clusterAction = $this->shouldBreakCluster($article, $insertedArticleClusterId, $clusterModel);
+        if ($clusterAction['action'] == 'break') {
             $clusterModel->deleteUserFromCluster($insertedArticleClusterId, $article->WardrobeId);
-            $clusterBroken = true;
-        }
-        if ($clusterBroken) {
-            $isAddedToACluster =  $this->handlePotentialClusterMerge($article, $clusterModel);
+            
+            // Revalidate the cluster after removing the user
+            $this->revalidateCluster($insertedArticleClusterId, $clusterModel);
+            
+            $isAddedToACluster = $this->handlePotentialClusterMerge($article, $clusterModel);
             if (!$isAddedToACluster) {
                 $newClusterId = $clusterModel->insert(['IsActive' => '1'], true);
                 $clusterModel->addNewUserToCluster($article->WardrobeId, current_user()->Id, $newClusterId);
             }
+        } elseif ($clusterAction['action'] == 'merge') {
+            $this->mergeClusters($insertedArticleClusterId, $clusterAction['mergeClusterId'], $clusterModel);
         }
     }
-
+    
     private function handleArticleNotInCluster($article, $clusterModel)
     {
         $isAddedToACluster = $this->handlePotentialClusterMerge($article, $clusterModel);
@@ -160,12 +161,50 @@ class ArticleModel extends Model
         $clusterArticles = $clusterModel->getClusterArticlesByClusterId($insertedArticleClusterId);
         foreach ($clusterArticles as $cArticle) {
             if ($cArticle->Brand == $article->Brand && $cArticle->Size != $article->Size) {
-                return true;
+                return ['action' => 'break'];
             }
         }
-        return false;
+
+        // Check for potential merge in other clusters
+        $clusters = $clusterModel->where('IsActive', 1)->findAll();
+        foreach ($clusters as $cluster) {
+            if ($cluster->Id != $insertedArticleClusterId) { // Ensure we're not checking the current cluster
+                $clusterArticles = $clusterModel->getClusterArticlesByClusterId($cluster->Id);
+                $canMerge = false;
+                foreach ($clusterArticles as $clsArticle) {
+                    if ($clsArticle->Brand == $article->Brand) {
+                        $canMerge = true;
+                        if ($clsArticle->Size != $article->Size) {
+                            // Same brand but different size, can't merge
+                            $canMerge = false;
+                            break;
+                        }
+                    }
+                }
+                if ($canMerge) {
+                    return ['action' => 'merge', 'mergeClusterId' => $cluster->Id];
+                }
+            }
+        }
+
+        return ['action' => 'none'];
     }
 
+
+    private function mergeClusters($clusterIdA, $clusterIdB, $clusterModel)
+    {
+        // Assuming you have a method to get all users from a cluster
+        $usersFromClusterB = $clusterModel->getClusterDataById($clusterIdB);
+
+        // Add all users from cluster B to cluster A
+        foreach ($usersFromClusterB as $user) {
+            $clusterModel->addNewUserToCluster($user->WardrobeId, $user->UserId, $clusterIdA);
+            $clusterModel->deleteUserFromCluster($clusterIdB, $user->WardrobeId);
+        }
+
+        // Deactivate cluster B (or delete, based on your requirement)
+        $clusterModel->update($clusterIdB, ['IsActive' => 0]);
+    }
     private function handlePotentialClusterMerge($article, $clusterModel)
     {
         $clusters = $clusterModel->where('IsActive', 1)->findAll();
@@ -184,5 +223,43 @@ class ArticleModel extends Model
         }
 
         return $isAddedToACluster;
+    }
+    private function revalidateCluster($clusterId, $clusterModel)
+    {
+        $usersInCluster = $clusterModel->getClusterDataById($clusterId);
+        $firstUser = array_shift($usersInCluster);
+        $firstUserArticles = $this->getAllArticlesByWardrobeId($firstUser->WardrobeId);
+
+        foreach ($usersInCluster as $user) {
+            $userArticles = $this->getAllArticlesByWardrobeId($user->WardrobeId);
+            $isMatching = $this->areArticlesMatching($firstUserArticles, $userArticles);
+            if (!$isMatching) {
+                // Remove the user from the cluster
+                $clusterModel->deleteUserFromCluster($clusterId, $user->WardrobeId);
+                // Try to merge the user into another cluster or create a new one
+                $isAddedToACluster = $this->handlePotentialClusterMerge($userArticles[0], $clusterModel); // Assuming each user has at least one article
+                if (!$isAddedToACluster) {
+                    $newClusterId = $clusterModel->insert(['IsActive' => '1'], true);
+                    $clusterModel->addNewUserToCluster($user->WardrobeId, $user->UserId, $newClusterId);
+                }
+            }
+        }
+    }
+
+    private function areArticlesMatching($articlesA, $articlesB)
+    {
+        foreach ($articlesA as $articleA) {
+            $matchingFound = false;
+            foreach ($articlesB as $articleB) {
+                if ($articleA->Brand == $articleB->Brand && $articleA->Size == $articleB->Size) {
+                    $matchingFound = true;
+                    break;
+                }
+            }
+            if (!$matchingFound) {
+                return false;
+            }
+        }
+        return true;
     }
 }
